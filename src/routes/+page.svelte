99f1654b6cf53ea8cf2c98bcfb146912caba3a19
Paulo Diogo <pndiogo@gmail.com>
2024-01-05 13:17:56 +0000

    feat: Add video background and update styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<svelte:head>
+  <link rel="preload" href="/videos/bkg-video.webm" as="style" />
+  <link rel="preload" href="/videos/bkg-video.mp4" as="video" />
+</svelte:head>
+
+<section class="main">
+  <video autoplay loop muted playsinline class="video-player">
+    <source src="/videos/bkg-video.webm" type="video/webm" />
+    <source src="/videos/bkg-video.mp4" type="video/mp4" />
+    <track kind="captions" />
+  </video>
+  <p>Hello</p>
+</section>
