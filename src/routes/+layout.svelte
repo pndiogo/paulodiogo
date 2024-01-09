ffafdb056320603f4b54037e9f4a00d7caee81d4
Paulo Diogo <pndiogo@gmail.com>
2024-01-09 10:27:41 +0000

    chore: Add speed insights to layout component and update package dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,10 @@
   import '../app.css';
   import { dev } from '$app/environment';
   import { inject } from '@vercel/analytics';
+  import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
   inject({ mode: dev ? 'development' : 'production' });
+  dev ? null : injectSpeedInsights();
 </script>
 
 <slot />
